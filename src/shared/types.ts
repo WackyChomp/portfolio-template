b425ac8b8d86b3623e166faad7009edfc9049b01
Wilson Chan <wilsonchan54@gmail.com>
2023-06-17 23:33:34 -0400

    Removed logos and code snippets from App.tsx / deleted App.css / created hooks folder with media query code snippet / created shared folder with enum type

---
+++ src/shared/types.ts
@@ -0,0 +1,11 @@
+export enum SelectedPage {
+    Home = 'home',
+    Technologies = 'technologies',
+    Projects = 'projects',
+    Timeline = 'timeline',
+    Benefits = 'benefits',
+    OurDomains = 'ourdomains',
+    Perks = 'perks',
+    Experts = 'experts',
+    ContactUs = 'contactus'
+  }
